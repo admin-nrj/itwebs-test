7fc25973ccb2ee07978283c84b93340e5fc37182
local <localhost@localhost>
2025-09-29 18:41:17 +0300

    added ConfigService and BooksController to demonstrate simple routing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { BooksController } from './books/books.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+  ],
+  controllers: [
+    AppController,
+    BooksController
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  app.enableCors({ origin: '*' });
+  app.setGlobalPrefix('api');
+  const port = configService.get<number>('API_PORT') || 3002;
+  await app.listen(port).then(() => console.log(`API service listening on port ${port}`));
 }
 bootstrap();
