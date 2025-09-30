ac559b2f5f97514185e0ea18b2571d214b983427
local <localhost@localhost>
2025-09-30 15:19:07 +0300

    removed unused files

--- src/modules/auth/decorators/current-user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { AuthUser } from '../interfaces/auth-user.interface';
-
-export const CurrentUser = createParamDecorator((_: unknown, ctx: ExecutionContext): AuthUser => {
-  const request = ctx.switchToHttp().getRequest<{ user: AuthUser }>();
-  return request.user;
-});
